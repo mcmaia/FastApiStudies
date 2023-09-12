d708b9e5441fe376e71cde84cad02efe79b9d7a2
mcmaia <marcelo.castromaia@gmail.com>
2023-09-11 21:24:27 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import Body, FastAPI
+
+app = FastAPI()
+
+
+BOOKS = [
+    {"title": "The Picture of Dorian Gray", "author": "Oscar Wilde", "year": "1890", "category": "Gothic Fiction"},
+    {"title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": "1925", "category": "Tragedy"},
+    {"title": "The Catcher in the Rye", "author": "J. D. Salinger", "year": "1951", "category": "Coming-of-age Fiction"},
+    {"title": "The Grapes of Wrath", "author": "John Steinbeck", "year": "1939", "category": "Realistic Fiction"},
+    {"title": "The Adventures of Huckleberry Finn", "author": "Mark Twain", "year": "1884", "category": "Adventure Fiction"},
+]
+
+#path parameter get method
+@app.get("/books")
+async def get_books():
+    return BOOKS
+
+# dynamic path
+@app.get("/books/{book_titlce}")
+async def get_books_dynamic(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+        
+#query parameter
+@app.get("/books/")
+async def get_book_author(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+# dynamic path and query parameter
+@app.get("/books/{book_author}/")
+async def get_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+            book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+#post method
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+    return new_book
